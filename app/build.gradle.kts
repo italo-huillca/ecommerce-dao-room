ef05632e21d32b44f344778f5acf72626c925c3e
Italo H <yhon.huillca@tecsup.edu.pe>
2025-05-06 14:13:52 -0500

    build: añadiendo dependencias de room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("kotlin-kapt")
 }
 
 android {
@@ -38,9 +39,13 @@ android {
         compose = true
     }
 }
+val room_version = "2.6.1"
+
 
 dependencies {
-
+    implementation("androidx.room:room-runtime:$room_version")
+     implementation("androidx.room:room-ktx:$room_version")
+     kapt("androidx.room:room-compiler:$room_version")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
